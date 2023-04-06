193e9957ef0576e66f579045af30bc66870e5f5b
Huseyin Gomleksizoglu <huseyin.gomleksizoglu@stonebranch.com>
2023-04-06 11:43:30 -0400

    Variables moved to variable file

--- aws_main.tf
+++ aws_main.tf
@@ -10,40 +10,6 @@ terraform {
 }
 
 # Defining Master count
-variable "access_key" {
-  sensitive = false
-  default = ""
-}
-
-
-# Defining Master count
-variable "secret_key" {
-  sensitive = false
-  default = ""
-}
-
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "ami" {
-  default = "ami-09d95fab7fff3776c"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-
-variable "tag" {
-    default = ""
-    type = string
-}
-
-variable "ssh-public-key" {
-    default = ""
-    type = string
-}
 
 
 provider "aws" {
